b8924386ae5d93d79d991530f007b6ad294637a8
yesseniarod <yessi.rod30@gmail.com>
2021-02-23 23:16:13 +0000

    when navigation button is clicked, homepage hidden. #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-// var $buttonList = document.querySelector('.button-list');
-// $buttonList.addEventListener('click', function(event) {
-//   if (event.target.getAttribute('class') === 'pictures') {
-//     console.log('Tailwags clicked');
-//   }
-// });
+var $buttonList = document.querySelector('.button-list');
+var $homePage = document.querySelector('.homepage');
+var $menuIcon = document.querySelector('.fas.fa-bars.hidden');
+
+$buttonList.addEventListener('click', function (event) {
+  if (event.target.getAttribute('class') === 'pictures') {
+    $homePage.classList.add('hide');
+    $menuIcon.classList.remove('hidden');
+  }
+});
