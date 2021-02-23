ae70d678c277357b18947ccf9113f58c910d0bda
yesseniarod <yessi.rod30@gmail.com>
2021-02-23 23:05:00 +0000

    listen for click events on navigation buttons. #1

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// var $buttonList = document.querySelector('.button-list');
+// $buttonList.addEventListener('click', function(event) {
+//   if (event.target.getAttribute('class') === 'pictures') {
+//     console.log('Tailwags clicked');
+//   }
+// });
