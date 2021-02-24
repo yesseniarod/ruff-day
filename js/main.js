ba5d1bdb3fa1d4a149fb114b1015b40511ae1d38
yesseniarod <yessi.rod30@gmail.com>
2021-02-24 00:53:57 +0000

    verify that every time tailwags is clicked, new dog image appears. #1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 var $buttonList = document.querySelector('.button-list');
 var $homePage = document.querySelector('.homepage');
 var $menuIcon = document.querySelector('.fas.fa-bars.hidden');
+var $placeHolder = document.querySelector('.place-holder');
+
+function getDogPicture() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    var $img = document.createElement('img');
+    $img.setAttribute('src', xhr.response.message);
+    $placeHolder.appendChild($img);
+
+  });
+  xhr.send();
+
+}
 
 $buttonList.addEventListener('click', function (event) {
   if (event.target.getAttribute('class') === 'pictures') {
     $homePage.classList.add('hide');
     $menuIcon.classList.remove('hidden');
+    getDogPicture();
   }
 });
