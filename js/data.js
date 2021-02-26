6ddc38d33dd5a36791b2d1f88c4f6542f9df2cb4
yesseniarod <yessi.rod30@gmail.com>
2021-02-26 19:55:46 +0000

    verify that new entries are saved to local storage. #3

--- js/data.js
+++ js/data.js
@@ -4,3 +4,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var pastData = localStorage.getItem('data-storage');
+if (pastData !== null) {
+  data = JSON.parse(pastData);
+}
+
+window.addEventListener('unload', function () {
+  var dataEntry = JSON.stringify(data);
+  localStorage.setItem('data-storage', dataEntry);
+});
