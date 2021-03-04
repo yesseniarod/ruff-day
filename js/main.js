ea20e87e631fc4db0c316825c1d09cda742c247b
yesseniarod <yessi.rod30@gmail.com>
2021-03-04 23:16:37 +0000

    changed variable name of menuIcon to homeIcon.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var $buttonList = document.querySelector('.button-list');
 var $homePage = document.querySelector('.homepage');
-var $menuIcon = document.querySelector('.fas.fa-home.hidden');
+var $homeIcon = document.querySelector('.fas.fa-home.hidden');
 var $placeHolder = document.querySelector('.place-holder');
 var $tailWags = document.querySelector('.tailwags');
 var $intro = document.querySelector('.full-col.intro');
@@ -61,9 +61,9 @@ function getQuote() {
   xhr2.send();
 }
 
-$menuIcon.addEventListener('click', function (event) {
+$homeIcon.addEventListener('click', function (event) {
   $homePage.classList.remove('hide');
-  $menuIcon.classList.add('hidden');
+  $homeIcon.classList.add('hidden');
   $tailWags.classList.add('hide');
   $stayPawsitive.classList.add('hide');
   $reflect.classList.add('hide');
@@ -76,19 +76,19 @@ $buttonList.addEventListener('click', function (event) {
 
   if (event.target.getAttribute('class') === 'pictures') {
     $tailWags.classList.remove('hide');
-    $menuIcon.classList.remove('hidden');
+    $homeIcon.classList.remove('hidden');
     getDogPicture();
     greeting();
   } else if (event.target.getAttribute('class') === 'quotes') {
     $stayPawsitive.classList.remove('hide');
-    $menuIcon.classList.remove('hidden');
+    $homeIcon.classList.remove('hidden');
     getQuote();
   } else if (event.target.getAttribute('class') === 'thoughts') {
     $reflect.classList.remove('hide');
-    $menuIcon.classList.remove('hidden');
+    $homeIcon.classList.remove('hidden');
   } else if (event.target.getAttribute('class') === 'entries') {
     $viewEntry.classList.remove('hide');
-    $menuIcon.classList.remove('hidden');
+    $homeIcon.classList.remove('hidden');
   } else {
     $homePage.classList.remove('hide');
   }
